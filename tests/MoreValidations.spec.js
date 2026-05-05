2603d391fadccfe96b6d59ef8c9bc072a68c4f2b
Ayan <ayanbhat312@gmail.com>
2026-05-05 17:08:55 +0530

    Added MoreValidations file which deals with pop ups, framelocators, hovers etc.

---
+++ tests/MoreValidations.spec.js
@@ -0,0 +1,21 @@
+const { test,expect } = require('@playwright/test')
+
+test("Pop Up Validation", async({ page })=>{
+
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    // await page.goto("https://google.com");
+    // await page.goBack();
+    // await page.goForward();
+    await expect(page.locator("#displayed-text")).toBeVisible;
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden;
+    page.on('dialog',dialog => dialog.accept());
+    await page.locator("#confirmbtn").click();
+    await page.locator("#mousehover").hover();
+    const framespage = page.frameLocator("#courses-iframe");
+    await framespage.locator("li a[href*='lifetime-access']:visible").click();
+    const subscribers = await framespage.locator(".text h2").textContent();
+    console.log(subscribers.split(" ")[1]); 
+
+
+});
