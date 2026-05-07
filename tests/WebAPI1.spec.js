496b925f0772f3382012d892bda51dbee647dba6
Ayan <ayanbhat312@gmail.com>
2026-05-07 12:49:03 +0530

    Api testing refactored with utils

--- tests/WebAPI1.spec.js
+++ tests/WebAPI1.spec.js
@@ -1,74 +1,28 @@
 const { test,expect,request } = require('@playwright/test')
+const {APiUtils} = require('./utils/APiUtils');
 const loginPayload = {userEmail:"ayan@example.com",userPassword:"Ayan@123"}
+const orderPayload = {orders: [{country: "Cuba", productOrderedId: "6960eae1c941646b7a8b3ed3"}]}
+let orderId
 let token;
+let response;
 
 test.beforeAll( async () => {
     const apiCall = await request.newContext();
-    const loginResponse = await apiCall.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-        {
-            data: loginPayload
-        })
-    expect(loginResponse.ok()).toBeTruthy();
-    const loginResponseJson = await loginResponse.json();
-    token = loginResponseJson.token;
-    console.log(token);
+    const apiUtils = new APiUtils(apiCall,loginPayload);
+    response = await apiUtils.createOrder(orderPayload);
 });
 
-test.beforeEach( () => {
-
-});
-
-
+//Creating an Order through API and validating it in the Web App
 test('API Login and PLace order', async ({ page }) => {
 
     await page.addInitScript(value => {
         window.localStorage.setItem('token',value);
-    }, token );
+    }, response.token );
 
 
    await page.goto("https://rahulshettyacademy.com/client");
-
    const email = "ayan@example.com";
    const productName = 'ZARA COAT 3';
-   const products = page.locator(".card-body");
-   await page.locator(".card-body b").first().waitFor();
-   const titles = await page.locator(".card-body b").allTextContents();
-   console.log(titles); 
-   const count = await products.count();
-   for (let i = 0; i < count; ++i) {
-      if (await products.nth(i).locator("b").textContent() === productName) {
-         //add to cart
-         await products.nth(i).locator("text= Add To Cart").click();
-         break;
-      }
-   }
- 
-   await page.locator("[routerlink*='cart']").click();
-   //await page.pause();
- 
-   await page.locator("div li").first().waitFor();
-   const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
-   expect(bool).toBeTruthy();
-   await page.locator("text=Checkout").click();
- 
-  await page.getByPlaceholder('Select Country').pressSequentially("ind", { delay: 150 }) 
-   const dropdown = page.locator(".ta-results");
-   await dropdown.waitFor();
-   const optionsCount = await dropdown.locator("button").count();
-   for (let i = 0; i < optionsCount; ++i) {
-      const text = await dropdown.locator("button").nth(i).textContent();
-      if (text === " India") {
-         await dropdown.locator("button").nth(i).click();
-         break;
-      }
-   }
- 
-   expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
-   await page.locator(".action__submit").click();
-   await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-   const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-   console.log(orderId);
- 
    await page.locator("button[routerlink*='myorders']").click();
    await page.locator("tbody").waitFor();
    const rows = await page.locator("tbody tr");
@@ -76,12 +30,13 @@ test('API Login and PLace order', async ({ page }) => {
  
    for (let i = 0; i < await rows.count(); ++i) {
       const rowOrderId = await rows.nth(i).locator("th").textContent();
-      if (orderId.includes(rowOrderId)) {
+      if (response.orderId.includes(rowOrderId)) {
          await rows.nth(i).locator("button").first().click();
          break;
       }
    }
    const orderIdDetails = await page.locator(".col-text").textContent();
-   expect(orderId.includes(orderIdDetails)).toBeTruthy();
+   await page.pause();
+   expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
  
 });
---
+++ tests/utils/APiUtils.js
@@ -0,0 +1,48 @@
+class APiUtils
+{
+    constructor(apiCall,loginPayload)
+    {
+        this.apiCall = apiCall;
+        this.loginPayload = loginPayload;
+    }
+
+    async getToken()
+    {
+        const loginResponse = await this.apiCall.post("https://rahulshettyacademy.com/api/ecom/auth/login",
+            {
+                data: this.loginPayload
+            })
+        const loginResponseJson = await loginResponse.json();
+        const token = loginResponseJson.token;
+        console.log(token);
+        return token;
+    }
+
+    async createOrder(orderPayload)
+    {
+
+        let response ={};
+        response.token = await this.getToken();
+        const orderResponse = await this.apiCall.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+            {
+                data: orderPayload,
+                headers: {
+                            'Authorization': response.token,
+                            'Content-Type': 'application/json'
+                        }
+            }
+        );
+
+        const orderResponseJson = await orderResponse.json();
+        console.log(orderResponseJson);
+        const orderId = orderResponseJson.orders[0];
+        response.orderId = orderId;
+        return response;
+    }
+
+}
+
+module.exports = {APiUtils};
+
+
+    
