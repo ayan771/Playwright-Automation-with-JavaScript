04ff39ba0dd511ffeec8a043caf236888cdff6fa
Ayan <ayanbhat312@gmail.com>
2026-05-13 11:48:31 +0530

    Added GitHub Repository Secrets to the Yaml file

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -1,6 +1,6 @@
-const { test,expect } = require('@playwright/test')
+const { test, expect } = require('@playwright/test')
 
-test("Pop Up Validation", async({ page })=>{
+test("Pop Up Validation", async ({ page }) => {
 
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     // await page.goto("https://google.com");
@@ -9,13 +9,26 @@ test("Pop Up Validation", async({ page })=>{
     await expect(page.locator("#displayed-text")).toBeVisible();
     await page.locator("#hide-textbox").click();
     await expect(page.locator("#displayed-text")).toBeHidden();
-    page.on('dialog',dialog => dialog.accept());
+    page.on('dialog', dialog => dialog.accept());
     await page.locator("#confirmbtn").click();
     await page.locator("#mousehover").hover();
     const framespage = page.frameLocator("#courses-iframe");
     await framespage.locator("li a[href*='lifetime-access']:visible").click();
     const subscribers = await framespage.locator(".text h2").textContent();
-    console.log(subscribers.split(" ")[1]); 
+    console.log(subscribers.split(" ")[1]);
 
 
+});
+
+test("Screenshot and Visual Comparison", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await page.screenshot({path: 'screenshot.png'})
+    await expect(page.locator("#displayed-text")).toBeHidden();
+});
+
+test.only('Visual Testing' , async ({page}) => {
+    await page.goto("https://flightware.com/");
+    expect(await page.screenshot()).toMatchSnapshot('landing.png');
 });
