21abe081a911473fc8c5c097a879c4ae86b869db
Ayan <ayanbhat312@gmail.com>
2026-06-11 15:22:49 +0530

    Merge Conflicts #2

--- playwright.config.js
+++ playwright.config.js
@@ -15,11 +15,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-<<<<<<< HEAD
-  // fullyParallel: true,
-=======
   fullyParallel: false,
->>>>>>> 6813f7cd9228062e0a32c89ab4d6d8a6bdd42b72
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
